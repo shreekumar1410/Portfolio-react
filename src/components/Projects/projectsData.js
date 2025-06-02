c86785379b22f48d938b0b5a1294cda96a3e84e9
shreekumar1410 <shreekumar1410@gmail.com>
2025-06-02 22:38:21 +0530

    FEAT: Added deatil of the projects.

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -1,37 +1,41 @@
+import Smart from "../../assets/images/smart.avif";
+import ODIR from "../../assets/images/ODIR.avif";
+import Animal from "../../assets/images/ANIMAL.jpeg";
+
 const projectsData = [
   {
     id: 1,
-    title: "Project 1",
-    description: "abc",
-    // img: "...",
+    title: "IOT BASED SMART IRRIGATION WITH WEED DETECTION USING ML",
+    description: "Redesign ITIC's company profile to effectively showcase its services in the environmental, healthcare, and warehousing sectors, ensuring comprehensive representation for diverse target audiences.",
+    img: Smart,
     // link: "...",
-    // github: "...",
+    github: "https://github.com/shreekumar1410/smart__irrigation",
     tags: ["React", "Node.js"],
-    category: "web" // Add this field
+    category: ["IoT","web-development"] // Add this field
   },
 
   {
     id: 2,
-    title: "Project 2",
-    description: "xc bnmk",
-    // img: "...",
+    title: "ODIR: SEEING THE BIG PICTURE FOR EYE HEALTH",
+    description: "Redesign Al Khudair International's company profile to effectively showcase its expertise in residential, commercial, and industrial projects.",
+    img: ODIR,
     // link: "...",
-    // github: "...",
+    github: "https://github.com/shreekumar1410/PBL-NT-GP-8511-1683280726",
     tags: ["React", "Node.js"],
-    category: "web" // Add this field
+    category: ["AI/ML","web-development"] // Add this field
   },
 
   {
     id: 3,
-    title: "Project 3",
-    description: "asdfghjk",
-    // img: "...",
+    title: "ANIMAL SPECIES DETECTION",
+    description: "Design branding for Leia Activewear, targeting women with a mission to empower them through high-quality activewear.",
+    img: Animal,
     // link: "...",
-    // github: "...",
+    github: "https://github.com/shreekumar1410/animal_prediction",
     tags: ["React", "Node.js"],
-    category: "web" // Add this field
+    category: ["AI/ML","web-development"] // Add this field
   },
-  // Other projects...
+    // Other projects...
 ];
 
 export default projectsData;
